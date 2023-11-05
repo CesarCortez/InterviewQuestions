41341a0994a373304094b4f1e4c62778aa6dfc2e
CesarCortez <cesar.josep@hotmail.com>
2023-11-05 07:59:13 -0600

    how to run the pokeCLI api

--- NodeJs/exercises/PokeAPI_Files/appCLI.js
+++ NodeJs/exercises/PokeAPI_Files/appCLI.js
@@ -11,12 +11,19 @@ console.log("0. Exit");
 
 switch (parseInt(process.argv[2])) {
   case 1:
+    // node appCLI.js 1
+    // node appCLI.js 1 20 0
+    // node appCLI.js 1 20 20
     getPokemon( process.argv[3], process.argv[4] );
     break;
   case 2:
+    // node appCLI.js 2 pikachu
+    // node appCLI.js 2 25
     getPokemonId(process.argv[3]);
     break;
   case 3:
+    // node appCLI.js 3 pikachu
+    // node appCLI.js 3 25
     getPokemonIdAndCreateFile(process.argv[3]);
     break;
   default:
